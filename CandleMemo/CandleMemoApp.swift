a600e61fa9a7ff0dbf0ce765d682311fc3ae9eb9
1145 <team_halves.0h@icloud.com>
2022-04-08 22:31:29 +0900

    Initial Commit

---
+++ CandleMemo/CandleMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CandleMemoApp.swift
+//  CandleMemo
+//
+//  Created by Hoen on 2022/04/08.
+//
+
+import SwiftUI
+
+@main
+struct CandleMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CandleMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CandleMemo
+//
+//  Created by Hoen on 2022/04/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
