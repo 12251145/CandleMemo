d73e50766e0dc7f7c2ba431bf5f5fa233dc5ed57
1145 <team_halves.0h@icloud.com>
2022-04-29 23:56:39 +0900

    디테일뷰 리스트 플로팅 버튼 삭제

--- CandleMemo/View/DetailView.swift
+++ CandleMemo/View/DetailView.swift
@@ -30,71 +30,12 @@ struct DetailView: View, FormatChanger {
             ZStack {
                 VStack {
                     CandleChartView(market: market, ticker: ticker)
-                    
-                    List {
-                        ForEach(0...10, id: \.self) { i in
-                            Button {
-                                
-                            } label: {
-                                VStack(alignment: .leading, spacing: 10) {
-                                    Text("🤯")
-                                        .font(.title2)
-                                    HStack(alignment: .firstTextBaseline) {
-                                        Text("2022/4/18 월")
-                                            .fontWeight(.semibold)
-                                        
-                                        Text("나는 망했다!!나는 망했다!!나는 망했다!!나는 망했다!!나는 망했다!!나는 망했다!!나는 망했다!!나는 망했다!!나는 망했다!!나는 망했다!!")
-                                            .font(.caption)
-                                            .lineLimit(1)
-                                            
-                                    }
-                                }
-                                .padding(.vertical, 10)
-                            }
-                            .listRowSeparator(.hidden)
-                        }
-                    }
-                    .listStyle(.plain)
-                    
                 }
-                
-                Button{
-                    
-                } label: {
-                    ZStack {
-                        Circle()
-                            .fill(.green)
-                            .frame(width: 60, height: 60)
-                            
-                        Image(systemName: "plus")
-                            .font(.title2)
-                            .foregroundColor(.black)
-                    }
-                }
-                .offset(x: (proxy.size.width - floatingButtonSize) / 2 - floatingButtonMarginx,
-                        y: (proxy.size.height - floatingButtonSize) / 2 - floatingButtonMarginy)
-                
-                Button{
-                    
-                } label: {
-                    ZStack {
-                        Circle()
-                            .fill(.white)
-                            .frame(width: 60, height: 60)
-                    
-                        Image(systemName: "magnifyingglass")
-                            .font(.title2)
-                            .foregroundColor(.black)
-                    }
-                }
-                .offset(x: (proxy.size.width - floatingButtonSize) / 2 - floatingButtonMarginx2,
-                        y: (proxy.size.height - floatingButtonSize) / 2 - floatingButtonMarginy2)
             }
         }
         .navTitle(cutKRW(from: market.code))
         .coinName(market.koreanName)
         .price(priceFormat(ticker.tradePrice))
-        
     }
 }
 
